cbb768b41d8a53decf6178b204b2101e6e79d00a
zegabr <jgsp2@cin.ufpe.br>
2019-08-01 19:59:59 -0300

    condicional melhorado

--- back-end/routes/cartela.js
+++ back-end/routes/cartela.js
@@ -15,11 +15,11 @@ router.put("/", function (req, res) {//atualizar arary de pintados da cartela pr
     let i = body.clickedLine
     let j = body.clickedColumn
     let num = body.Numero
-    if (i!=null && j!=null && num) {
+    if (i!=null && j!=null && num!=null) {
         let acertou = cartelaModel.updateCartela(i,j,num)//retorna true se acertou o numero
         if(acertou) res.send({message: "Acertou"})
         else{
-            res.send({ message: "Errou o numero" })
+            res.send({ message: "Errou" })
         }
     } else {
         console.log(i,j,num)
