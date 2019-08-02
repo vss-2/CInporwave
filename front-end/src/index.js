65fab4c04fa06f7d495c87e7f7bc96f27ecb64ee
Josue Fidelis de Oliveira Neto <jfon@WINDOWS.CIN.UFPE.BR>
2019-08-02 11:36:15 -0300

    mudança

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AppHome from 'AppHome.js';
-import bingo from 'bingo.js';
+import Game from 'bingo.js';
 
 
 ReactDOM.render(
