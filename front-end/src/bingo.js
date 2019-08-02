244597e4978b40fc562428ec27f93580d9145b05
Josue Fidelis de Oliveira Neto <jfon@WINDOWS.CIN.UFPE.BR>
2019-08-02 14:12:25 -0300

    Bingo pseudo-funcional

--- front-end/src/bingo.js
+++ front-end/src/bingo.js
@@ -13,7 +13,7 @@ import axios from 'axios';
   }*/
   function Square (props) {
       return (
-          <button className = "square" onClick={props.onClick} id = {props.id}>
+          <button className = {props.id} onClick={props.onClick} id = {props.id}>
               {props.value}
           </button>
       );
@@ -21,47 +21,49 @@ import axios from 'axios';
   
   class Board extends React.Component {
       
-    renderSquare(i) {
-        return <Square value = {this.props.squares[i]} onClick={() => this.props.onClick(i)} id = {this.props.id}/>;
+    renderSquare(i, color) {
+        return <Square value = {this.props.squares[i]} onClick={() => this.props.onClick(i)} id = {color}/>;
     }
-  
+    
+    
     render() {
+        const squareCor = this.props.squareColor;
         return (
             <div>
             <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
+                {this.renderSquare(0, squareCor[0])}
+                {this.renderSquare(1, squareCor[1])}
+                {this.renderSquare(2, squareCor[2])}
+                {this.renderSquare(3, squareCor[3])}
+                {this.renderSquare(4, squareCor[4])}
             </div>
             <div className="board-row">
-                {this.renderSquare(5)}
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-                {this.renderSquare(9)}
+                {this.renderSquare(5, squareCor[5])}
+                {this.renderSquare(6, squareCor[6])}
+                {this.renderSquare(7, squareCor[7])}
+                {this.renderSquare(8, squareCor[8])}
+                {this.renderSquare(9, squareCor[9])}
             </div>
             <div className="board-row">
-                {this.renderSquare(10)}
-                {this.renderSquare(11)}
-                {this.renderSquare(12)}
-                {this.renderSquare(13)}
-                {this.renderSquare(14)}
+                {this.renderSquare(10, squareCor[10])}
+                {this.renderSquare(11, squareCor[11])}
+                {this.renderSquare(12, squareCor[12])}
+                {this.renderSquare(13, squareCor[13])}
+                {this.renderSquare(14, squareCor[14])}
             </div>
             <div className="board-row">
-                {this.renderSquare(15)}
-                {this.renderSquare(16)}
-                {this.renderSquare(17)}
-                {this.renderSquare(18)}
-                {this.renderSquare(19)}
+                {this.renderSquare(15, squareCor[15])}
+                {this.renderSquare(16, squareCor[16])}
+                {this.renderSquare(17, squareCor[17])}
+                {this.renderSquare(18, squareCor[18])}
+                {this.renderSquare(19, squareCor[19])}
             </div>
             <div className="board-row">
-                {this.renderSquare(20)}
-                {this.renderSquare(21)}
-                {this.renderSquare(22)}
-                {this.renderSquare(23)}
-                {this.renderSquare(24)}
+                {this.renderSquare(20, squareCor[20])}
+                {this.renderSquare(21, squareCor[21])}
+                {this.renderSquare(22, squareCor[22])}
+                {this.renderSquare(23, squareCor[23])}
+                {this.renderSquare(24, squareCor[24])}
             </div>
             </div>
         );
@@ -74,9 +76,12 @@ class Game extends React.Component {
         super(props);
         this.state = {
             squares: Array(25).fill(null), 
-            xIsNext: true,
+            acerto: 'square',
+            squareColor: Array(25).fill('square'),
         };
     }
+
+    
 
     init () {
         axios.get('http://localhost:3333/cartelas/novacartela')
@@ -121,30 +126,27 @@ class Game extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        
+        const SquareCor = this.state.squareColor.slice();
         
         /*if (/*calculateWinner(squares) || squares[i]) { MUDAR para não apertar botão já pressionado
             return;
         }*/
 
-
-        //if (this.updateCartela() === 'Errou') {
+        //let cartela = this.updateCartela();
+        //if (cartela === 'Errou') {
+        //    alert("Tente novamente :(");
         //    return;
-        //} else if (this.updateCartela() === 'Acertou') {
+        //} else if (cartela === 'Acertou') {
             //squares[i] = this.state.xIsNext? 'X' : 'O';
+            SquareCor[i] = 'squareRight'
             this.setState({
                 squares: squares,
-                xIsNext: !this.state.xIsNext
+                acerto: 'squareRight',
+                squareColor: SquareCor
             });
         //}
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step%2) === 0,
-        });
-    }
     
     componentDidMount () {
         if (localStorage.getItem('cartela')) {
@@ -174,14 +176,14 @@ class Game extends React.Component {
         //    status = 'Winner: ' + winner; 
         //} 
         
-        let squareState = "square";
         return (
         <div className="game">
           <div className="game-board">
             <Board
                 squares = {square}
                 onClick = {(i) => this.handleClick(i)}
-                id = {squareState}
+                id = {this.state.acerto}
+                squareColor = {this.state.squareColor}
             />
           </div>
           <div className="game-info">
