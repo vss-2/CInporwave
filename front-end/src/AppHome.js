b1e07512d16ec7581c0ad462dcf52d7707414540
Josue Fidelis de Oliveira Neto <jfon@WINDOWS.CIN.UFPE.BR>
2019-08-02 09:40:24 -0300

    criação da pagina de bingo

---
+++ front-end/src/AppHome.js
@@ -0,0 +1,34 @@
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+      <div className="nav">
+        <input type="checkbox" id="nav-check" />
+        <div className="nav-header">
+          <div className="nav-title">
+            CInporwave
+          </div>
+        </div>
+        <div className="nav-btn">
+          <label htmlFor="nav-check">
+            <span />
+            <span />
+            <span />
+          </label>
+        </div>
+        <div className="nav-links">
+          <a href="" target="_blank">Atividades</a>
+          <a href="" target="_blank">Configurações</a>
+          <a href="" target="_blank">Sobre nós</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
