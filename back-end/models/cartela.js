94bcebc464c28c7b620c4699b4758ae82f40953b
vss-2 <32624576+vss-2@users.noreply.github.com>
2019-10-26 21:18:28 -0300

    Legibility update
    Coerência no uso de ";" e correção de erros em português.

--- back-end/models/cartela.js
+++ back-end/models/cartela.js
@@ -1,8 +1,8 @@
-let axios = require("axios")
+let axios = require("axios");
 
 const auxCartela = {
-    "qlinhas":5,
-    "qcolunas":5,
+    "qlinhas":  5,
+    "qcolunas": 5,
     "tabela":[  [1,2,3,4,5],
                 [6,7,8,9,10],
                 [11,12,13,14,15],
@@ -15,10 +15,10 @@ const auxCartela = {
                 [0,0,0,0,0]  ],
 }
 
-let myCartela = auxCartela
+let myCartela = auxCartela;
 
 function getCartela(){
-    return myCartela
+    return myCartela;
 }
 
 /**
@@ -26,7 +26,7 @@ function getCartela(){
  * Using Durstenfeld shuffle algorithm.
  */
 function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
+    for(var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
         var temp = array[i];
         array[i] = array[j];
@@ -35,80 +35,116 @@ function shuffleArray(array) {
 }
 
 function createCartela5por5(){
-    let qlinhas = 5
-   let qcolunas = 5
-    let numbers=[]
-   for(let i=0;i<100;i++)numbers.push(i)
-   let newCartela=[]
-   for(let i=0;i<qlinhas;i++){
-       let linha=[]
-       for(let j=0;j<qcolunas;j++){
-           shuffleArray(numbers)
-           linha.push(numbers.pop())
+   let qlinhas  = 5;
+   let qcolunas = 5;
+   let numbers  = [];
+   
+   for(let i = 0; i < 100; i++){
+       numbers.push(i);
+   }
+    
+   let newCartela = [];
+   for(let i=0; i < qlinhas; i++){
+       let linha = [];
+       for(let j=0; j < qcolunas; j++){
+           shuffleArray(numbers);
+           linha.push(numbers.pop());
         }
-        newCartela.push(linha)
+        newCartela.push(linha);
     }
     
-    let newUsados=[]
-    let aux = []
-    for(let i=0;i<qcolunas;i++) aux.push(0)
-    for(let i=0;i<qlinhas;i++) newUsados.push(aux)
+    let newUsados = [];
+    let aux = [];
+    
+    for(let i=0;i < qcolunas; i++){ 
+        aux.push(0);
+    }
+    
+    for(let i=0; i < qlinhas; i++){
+        newUsados.push(aux);
+    }
    
-    myCartela.qlinhas = qlinhas
-    myCartela.qcolunas = qcolunas
-    myCartela.tabela = newCartela
-    myCartela.usados = newUsados
-    return myCartela
+    myCartela.qlinhas = qlinhas;
+    myCartela.qcolunas = qcolunas;
+    myCartela.tabela = newCartela;
+    myCartela.usados = newUsados;
+    
+    return myCartela;
 }
 
 
-function createCartela(){//gerar randomicamente uma nova
-   let qlinhas = 0
-   let qcolunas = 0
-    while(qlinhas<2) qlinhas=Math.floor((Math.random() * 10) + 1);
-    while(qcolunas<2)  qcolunas=Math.floor((Math.random() * 10) + 1);
-   //console.log(qlinhas,qcolunas) ok
-   let numbers=[]
-   for(let i=0;i<100;i++)numbers.push(i)
-   let newCartela=[]
-   for(let i=0;i<qlinhas;i++){
-       let linha=[]
-       for(let j=0;j<qcolunas;j++){
-           shuffleArray(numbers)
-           linha.push(numbers.pop())
-        }
-        newCartela.push(linha)
+function createCartela(){
+   // Descrição: Gerar randomicamente uma nova cartela
+   
+    let qlinhas = 0;
+    let qcolunas = 0;
+    while(qlinhas < 2){
+        qlinhas=Math.floor((Math.random() * 10) + 1);
+    }
+    while(qcolunas < 2){
+        qcolunas=Math.floor((Math.random() * 10) + 1);
     }
     
-    let newUsados=[]
-    let aux = []
-    for(let i=0;i<qcolunas;i++) aux.push(0)
-    for(let i=0;i<qlinhas;i++) newUsados.push(aux)
+    //console.log(qlinhas,qcolunas) ok
+
+    let numbers = [];
+    
+    for(let i = 0; i < 100; i++){
+        numbers.push(i);
+    }
+    
+    let newCartela = [];
+    
+    for(let i = 0; i < qlinhas; i++){
+       let linha = [];
+       for(let j = 0; j < qcolunas; j++){
+           shuffleArray(numbers);
+           linha.push(numbers.pop());
+       }
+       newCartela.push(linha);
+    }
+    
+    let newUsados = [];
+    let aux = [];
+    
+    for(let i = 0; i < qcolunas; i++){
+        aux.push(0);
+    }
+    
+    for(let i = 0; i < qlinhas; i++){
+        newUsados.push(aux);
+    }
    
-    myCartela.qlinhas = qlinhas
-    myCartela.qcolunas = qcolunas
-    myCartela.tabela = newCartela
-    myCartela.usados = newUsados
-    return myCartela
+    myCartela.qlinhas = qlinhas;
+    myCartela.qcolunas = qcolunas;
+    myCartela.tabela = newCartela;
+    myCartela.usados = newUsados;
+    return myCartela;
 }
 
 function updateCartela(linha, coluna, num){
+    
     //console.log(linha,coluna,num)
-    let tab = myCartela.tabela
-    let qlinhas = myCartela.qlinhas
-    let qcolunas = myCartela.qcolunas
+    
+    let tab = myCartela.tabela;
+    let qlinhas = myCartela.qlinhas;
+    let qcolunas = myCartela.qcolunas;
+    
     //console.log(tab[0].length)
-    if(linha < qlinhas && linha>=0 && coluna < qcolunas && coluna>=0){//indice valido
-        let usados = myCartela.usados
-        if(usados[linha][coluna]==0 && tab[linha][coluna]==num){
-            console.log("acertou")
-            myCartela.usados[linha][coluna]=1
-            console.log("posicao",linha,coluna,"foi atualizada")
-            return true
+    
+    if(linha < qlinhas && linha >= 0 && coluna < qcolunas && coluna >= 0){
+    //indice valido
+        let usados = myCartela.usados;
+        
+        if(usados[linha][coluna] == 0 && tab[linha][coluna] == num){
+            console.log("Acertou.");
+            myCartela.usados[linha][coluna] = 1;
+            console.log("Posição", linha, coluna, "foi atualizada.");
+            return true;
         }
 
     }
-    return false
+    return false;
 }
 
 module.exports = {
